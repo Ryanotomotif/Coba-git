32af4d6440d05163189b3edf7d7bab9914f22d07
Ryanotomotif <rgunawan08@student.ciputra.ac.id>
2021-12-03 09:53:53 +0700

    add input nama

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace Coba_git
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
-			Console.WriteLine("wkwkwkwkwkwk");
+			Console.WriteLine("Masukkan nama :");
+			string nama = Console.ReadLine();
+
 		}
 	}
 }
